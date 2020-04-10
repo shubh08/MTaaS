6f7a0d3718a9c4505bf0c69821d0b2a5123256d2
Mukesh Mogal <34641662+MukeshMogal@users.noreply.github.com>
2020-04-09 20:17:28 -0700

    create run function added

--- Backend/src/routes/testRun.js
+++ Backend/src/routes/testRun.js
@@ -46,3 +46,36 @@ const storage = multer.diskStorage({
       
     }
 })
+
+const upload = multer({storage:storage})
+
+router.post('/dummyRun', function (req, res, next) {
+console.log('herere')
+console.log(req.file)
+console.log(req.file.path)
+res.send('fineshed')
+})
+
+router.post('/createRun',upload.single('file'), function (req, res, next) {
+createRun(req,res)
+})
+
+createRun = async (req,res)=>{
+    console.log('Inside createRun');
+    console.log(req.body)
+    console.log(req.file.path)
+    
+    const userName=req.body.userName
+    const projectName=req.body.projectName
+    const runname=req.body.runname
+    const appFileName=req.body.appFileName
+    const appFileType=req.body.appFileType
+    const devicePoolName=req.body.devicePoolName
+    const devicePoolARNs=req.body.devicePoolARNs
+    let DEVICE_POOL_ARN= 'arn:aws:devicefarm:us-west-2::devicepool:082d10e5-d7d7-48a5-ba5c-b33d66efa1f5'
+    const testType=req.body.testType
+    const testPackageFileName='zip-with-dependencies.zip'
+    const testPackageFileType=req.body.testPackageFileType
+
+    //Array of test file names that need to be zipped
+   // const testFileNames=JSON.parse(req.body.testFileNames)
