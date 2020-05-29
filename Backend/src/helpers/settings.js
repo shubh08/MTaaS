e0a5bdee89c1f1d0d11716c8f30deb2492a2a42a
nayakmitra7 <55523134+nayakmitra7@users.noreply.github.com>
2020-05-29 11:19:34 -0700

    Update settings.js

--- Backend/src/helpers/settings.js
+++ Backend/src/helpers/settings.js
@@ -1,5 +1,5 @@
 'use strict';
 module.exports = {
-    mongoURI : 'mongodb+srv://mtaas:mtaas@cluster0-aafyt.mongodb.net/mtaasTest?retryWrites=true&w=majority',
+    mongoURI : '',
     secretOrKey : ''
 };
